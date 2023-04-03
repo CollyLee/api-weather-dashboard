bebd47ed484f93775e9e4cebbe05f7d0dcce9a66
CollyLee <clh5160@gmail.com>
2023-04-03 17:43:24 -0400

    added local storage, added current day forecast details, finished API calls

--- js/script.js
+++ js/script.js
@@ -1,15 +1,6 @@
 // URL for the API info: https://openweathermap.org/forecast5
 
-// base URL to use for calls: https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key} (replace the curlies with the data you need)
-
-// Info on how to use OpenWeather ------------------------------------
-// lat, lon:	required	Geographical coordinates (latitude, longitude).
-
-// appid:	required	Your unique API key (you can always find it on your account page under the "API key" tab)
-
 // units:	optional	Units of measurement. standard, metric and imperial units are available. If you do not use the units parameter, standard units will be applied by default. Learn more
-
-// need to tie each city to a lat and lon
 
 // store recent searches in localstorage
 
@@ -24,25 +15,43 @@ var citySearchBar = document.querySelector('#city-search')
 var citySearchInput = document.querySelector('#city-search-input')
 var citySearchHistory = document.querySelector('#search-history')
 var currentDayForecast = document.querySelector('#current-day')
-var futureForecast = document.querySelector('#future-forecast')
+var futureForecast = document.getElementsByClassName('.five-day')
 
 // -----------------------------------------------------------------------------------------------
 // function that makes history divs sortable cards
 
 $(function () {
-  $("#sortable").sortable();
+  $(".sortable").sortable();
 });
+
+var init = function () {
+
+  var cityHistoryArray = JSON.parse(localStorage.getItem("city-history"))
+
+  if (cityHistoryArray !== null) {
+
+    for (var i = 0; i < cityHistoryArray.length; i++) {
+      var searchHistoryButton = document.createElement('button');
+      searchHistoryButton.textContent = cityHistoryArray[i];
+      citySearchHistory.appendChild(searchHistoryButton);
+    }
+  } else {
+    return
+  }
+}
 
 
 // -----------------------------------------------------------------------------------------------
 // search button function
 
-var searchHistory = [];
+var cityHistory = [];
 
 var citySearch = function (event) {
   event.preventDefault();
-
+  
   var city = citySearchInput.value;
+  cityHistory.push(city);
+  localStorage.setItem("city-history", JSON.stringify(cityHistory));
 
   var searchHistoryButton = document.createElement('button');
 
@@ -56,14 +65,24 @@ var citySearch = function (event) {
 }
 
 // -----------------------------------------------------------------------------------------------
+// historical search button function
+
+var cityHistorySearch = function(event) {
+  event.preventDefault();
+  var city = event.target.innerHTML;
+  getWeather(city);
+
+}
+
+// -----------------------------------------------------------------------------------------------
 // function to put together the API URL and run the search
 
-var getWeather = function () {
+var getWeather = function (city) {
 
   // -----------------------------------------------------------------------------------------------
   // function to pull the lat/long stats of the city
 
-  var cityStatsAPI = "http://api.openweathermap.org/geo/1.0/direct?q=" + "Chicago" + "&limit=5&appid=f21a10bf49ac32a7d097c29cd473c163"
+  var cityStatsAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=f21a10bf49ac32a7d097c29cd473c163";
 
   fetch(cityStatsAPI)
 
@@ -72,28 +91,48 @@ var getWeather = function () {
     })
 
     .then(function (data) {
-      console.log(data);
+      console.log("THE CURRENT FORECAST IS:  ", data);
+      currentDayForecast.children[0].textContent = dayjs().format('ddd M/D/YYYY');
+      currentDayForecast.children[1].textContent = "Current Temp: " + data.main.temp + "F"
+      currentDayForecast.children[2].textContent = "Current Wind Speed: " + data.wind.speed + "mph";
+      currentDayForecast.children[3].textContent = "Current Humidity: " + data.main.humidity + "%";
+
+      getFutureForecast(data);
+
     })
 
 
   // -----------------------------------------------------------------------------------------------
-  // fetch to pull weather stats using prev fetch data
+  // fetch to pull future weather stats using prev fetch data
+  var getFutureForecast = function (data) {
 
-  var apiURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + data.name + "," + data.state + "," + data.country + "&limit=" + 6 + "&appid=f21a10bf49ac32a7d097c29cd473c163";
+    // creates two variables by pointing to the location in the object
+    var { lat, lon } = data.coord
+    var apiURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=f21a10bf49ac32a7d097c29cd473c163`
 
-  fetch(apiURL)
+    fetch(apiURL)
 
-    .then(function (response) {
-      return response.json();
-    })
+      .then(function (response) {
+        return response.json();
+      })
 
-    .then(function (data) {
-      console.log(data);
-    })
+      .then(function (forecastData) {
+        console.log("THE FUTURE FORECAST IS: ", forecastData);
+
+        for (var i = 0; i < 5; i++)
+        futureForecast[i].children[1].textContent = "Testing 1";
+        futureForecast[i].children[2].textContent = "Testing 2";
+        futureForecast[i].children[3].textContent = "Testing 3";
+
+      })
+
+  }
 }
+
+init();
 
 // -----------------------------------------------------------------------------------------------
 // event listeners
 
-// .addEventListener('click', citySearch)
+citySearchHistory.addEventListener('click', cityHistorySearch)
 citySearchBar.addEventListener('submit', citySearch)
